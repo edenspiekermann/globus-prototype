a6fefc6f0c77f7eeffa1e972f8332d539b860f89
Gustav Pursche <g.pursche@de.edenspiekermann.com>
2018-04-25 14:15:30 +0200

    Header: improve spacing on small screens

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -10,22 +10,32 @@ export default () => (
         background-color: black;
         color: white;
         font-family: ${tokens.fonts.gill.family};
-        padding: 1rem;
+        padding: 15px 15px 5px 15px;
         text-align: center;
+      }
+
+      @media ${tokens.mq.desktop} {
+        .header {
+          padding-bottom: 20px;
+          padding-top: 30px;
+        }
       }
 
       .header__title {
         color: currentColor;
         display: block;
-        font-size: 2.5rem;
+        font-size: 34px;
         font-weight: bold;
-        margin-top: 10px;
+        line-height: 1;
+        margin-bottom: 0;
+        margin-top: 0;
         text-decoration: none;
         text-transform: uppercase;
       }
 
       @media ${tokens.mq.desktop} {
         .header__title {
+          font-size: 38px;
           margin-bottom: 10px;
           margin-top: 0;
         }
