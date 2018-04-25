6bd92d622c03e6a0f7cf3562e8e7ddd252956fe1
Gustav Pursche <g.pursche@de.edenspiekermann.com>
2018-04-25 16:44:04 +0200

    Intro: split out most styles

--- components/intro/index.jsx
+++ components/intro/index.jsx
@@ -5,9 +5,7 @@ import IntroImage from './image';
 import IntroTitle from './title';
 
 import tokens from '../../tokens';
-
-const IMAGE_SIZE_DESKTOP = '50%';
-const IMAGE_SIZE_LARGE = '45%';
+import styles from './styles';
 
 export default ({
   title,
@@ -21,65 +19,12 @@ export default ({
 }) => (
   <Fragment>
     <section className={`intro ${reversed ? 'intro--reversed' : ''}`}>
+      <style jsx>{styles}</style>
+
       <style jsx>{`
-        .intro {
-          margin-bottom: 80px;
-          overflow-x: hidden;
-          position: relative;
-        }
-
-        @media ${tokens.mq.desktop} {
-          .intro {
-            margin-bottom: 200px;
-          }
-        }
-
         .intro__title-container {
           background-color: ${color};
           padding: 20px 40px 300px 40px;
-        }
-
-        @media ${tokens.mq.desktop} {
-          .intro__title-container {
-            background-color: transparent;
-            margin-left: calc(${IMAGE_SIZE_DESKTOP} - 100px);
-            padding-bottom: 0;
-            position: relative;
-            z-index: 2;
-          }
-        }
-
-        @media ${tokens.mq.large} {
-          .intro__title-container {
-            margin-left: calc(${IMAGE_SIZE_LARGE} - 150px);
-          }
-        }
-
-        .intro--reversed .intro__title-container {
-          margin-bottom: 80px;
-          padding-bottom: 0;
-          position: relative;
-          z-index: 2;
-        }
-
-        @media ${tokens.mq.desktop} {
-          .intro--reversed .intro__title-container {
-            margin-left: 100px;
-            margin-right: calc(${IMAGE_SIZE_DESKTOP} - 100px);
-          }
-        }
-
-        @media ${tokens.mq.large} {
-          .intro--reversed .intro__title-container {
-            margin-left: 100px;
-            margin-right: calc(${IMAGE_SIZE_LARGE} - 100px);
-          }
-        }
-
-        .intro__image-container {
-          margin: -280px 60px 20px 120px;
-          overflow-y: hidden;
-          z-index: 1;
         }
 
         @media ${tokens.mq.desktop} {
@@ -98,22 +43,6 @@ export default ({
           }
         }
 
-        .intro--reversed .intro__image-container {
-          margin: 0;
-          position: absolute;
-          right: 0;
-          top: 0;
-          transform: translateX(50%);
-        }
-
-        @media ${tokens.mq.desktop} {
-          .intro--reversed .intro__image-container {
-            height: 100%;
-            left: auto;
-            transform: none;
-          }
-        }
-
         @media ${tokens.mq.large} {
           .intro__image-container {
             background-image: linear-gradient(
@@ -121,37 +50,6 @@ export default ({
               ${color} 80%,
               transparent 0
             );
-          }
-        }
-
-        .intro__content {
-          margin-left: 120px;
-          margin-right: 60px;
-          position: relative;
-          z-index: 2;
-        }
-
-        @media ${tokens.mq.desktop} {
-          .intro__content {
-            margin-left: calc(${IMAGE_SIZE_DESKTOP} + 100px);
-            max-width: 700px;
-          }
-        }
-
-        @media ${tokens.mq.large} {
-          .intro__content {
-            margin-left: calc(${IMAGE_SIZE_LARGE} + 100px);
-          }
-        }
-
-        .intro--reversed .intro__content {
-          margin-left: 40px;
-        }
-
-        @media ${tokens.mq.desktop} {
-          .intro--reversed .intro__content {
-            margin-left: 360px;
-            max-width: 900px;
           }
         }
       `}</style>
@@ -168,7 +66,6 @@ export default ({
         <IntroContent text={text} />
       </div>
     </section>
-
     {children}
   </Fragment>
 );
---
+++ components/intro/styles.js
@@ -0,0 +1,110 @@
+import css from 'styled-jsx/css';
+
+import tokens from '../../tokens';
+
+const IMAGE_SIZE_DESKTOP = '50%';
+const IMAGE_SIZE_LARGE = '45%';
+
+export default css`
+  .intro {
+    margin-bottom: 80px;
+    overflow-x: hidden;
+    position: relative;
+  }
+
+  @media ${tokens.mq.desktop} {
+    .intro {
+      margin-bottom: 200px;
+    }
+  }
+
+  @media ${tokens.mq.desktop} {
+    .intro__title-container {
+      background-color: transparent;
+      margin-left: calc(${IMAGE_SIZE_DESKTOP} - 100px);
+      padding-bottom: 0;
+      position: relative;
+      z-index: 2;
+    }
+  }
+
+  @media ${tokens.mq.large} {
+    .intro__title-container {
+      margin-left: calc(${IMAGE_SIZE_LARGE} - 150px);
+    }
+  }
+
+  .intro--reversed .intro__title-container {
+    margin-bottom: 80px;
+    padding-bottom: 0;
+    position: relative;
+    z-index: 2;
+  }
+
+  @media ${tokens.mq.desktop} {
+    .intro--reversed .intro__title-container {
+      margin-left: 100px;
+      margin-right: calc(${IMAGE_SIZE_DESKTOP} - 100px);
+    }
+  }
+
+  @media ${tokens.mq.large} {
+    .intro--reversed .intro__title-container {
+      margin-left: 100px;
+      margin-right: calc(${IMAGE_SIZE_LARGE} - 100px);
+    }
+  }
+
+  .intro__image-container {
+    margin: -280px 60px 20px 120px;
+    overflow-y: hidden;
+    z-index: 1;
+  }
+
+  .intro--reversed .intro__image-container {
+    margin: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transform: translateX(50%);
+  }
+
+  @media ${tokens.mq.desktop} {
+    .intro--reversed .intro__image-container {
+      height: 100%;
+      left: auto;
+      transform: none;
+    }
+  }
+
+  .intro__content {
+    margin-left: 120px;
+    margin-right: 60px;
+    position: relative;
+    z-index: 2;
+  }
+
+  @media ${tokens.mq.desktop} {
+    .intro__content {
+      margin-left: calc(${IMAGE_SIZE_DESKTOP} + 100px);
+      max-width: 700px;
+    }
+  }
+
+  @media ${tokens.mq.large} {
+    .intro__content {
+      margin-left: calc(${IMAGE_SIZE_LARGE} + 100px);
+    }
+  }
+
+  .intro--reversed .intro__content {
+    margin-left: 40px;
+  }
+
+  @media ${tokens.mq.desktop} {
+    .intro--reversed .intro__content {
+      margin-left: 360px;
+      max-width: 900px;
+    }
+  }
+`;
