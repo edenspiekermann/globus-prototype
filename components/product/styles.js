bb0c14a6d131efe1fbf91f59fde7d0c99b398925
AinzHansen <info@arneschog.de>
2018-05-02 12:32:57 +0200

    Update type styling in product

--- components/product/styles.js
+++ components/product/styles.js
@@ -13,12 +13,14 @@ export default css`
     font-size: 16px;
     margin-bottom: 0;
     text-transform: uppercase;
+    font-weight: normal;
   }
 
   .product__type {
     display: block;
     font-weight: normal;
     text-transform: none;
+    color: gray;
   }
 
   .product__price {
