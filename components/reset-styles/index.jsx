9150443631169b3bc00a3a978df88d3c915e8914
Gustav Pursche <g.pursche@de.edenspiekermann.com>
2018-05-02 13:50:41 +0200

    Make sure, there will never be a horizontal scrollbar

--- components/reset-styles/index.jsx
+++ components/reset-styles/index.jsx
@@ -10,6 +10,7 @@ export default () => (
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
       margin: 0;
+      overflow-x: hidden;
       padding: 0;
     }
 
