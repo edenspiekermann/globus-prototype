ea85e9714048980003e617405f516805043a8048
AinzHansen <info@arneschog.de>
2018-05-15 11:47:44 +0200

    Add correct products for in the mood

--- data/in-the-mood.js
+++ data/in-the-mood.js
@@ -10,6 +10,10 @@ export default {
           image: {
             url: '/static/images/california_chair-dress.png',
             alt: 'Under the desert sun'
+          },
+
+          style: {
+            padded: false
           }
         }
       ]
@@ -35,30 +39,30 @@ export default {
             products: [
               {
                 image: {
-                  url: '/static/images/logomania_3.jpg',
+                  url: '/static/images/nachthemd_3.jpg',
                   alt: 'Dummy image'
                 },
-                title: 'Calvin Klein',
-                type: 'Bikini Oberteil',
-                price: '79.90'
+                title: 'Globus Essentials',
+                type: 'Nachthemd',
+                price: '69.90'
               },
               {
                 image: {
-                  url: '/static/images/logomania_2.jpg',
+                  url: '/static/images/nachthemd_2.jpg',
                   alt: 'Dummy image'
                 },
-                title: 'FILA',
-                type: 'Athleisure-langarm Shirt',
-                price: '89.90'
+                title: 'Globus Essentials',
+                type: 'Pyjama Oberteil',
+                price: '49.90'
               },
               {
                 image: {
-                  url: '/static/images/logomania_1.jpg',
+                  url: '/static/images/nachthemd_1.jpg',
                   alt: 'Dummy image'
                 },
-                title: 'FILA',
-                type: 'Gürteltasche',
-                price: '19.90'
+                title: 'Globus Essentials',
+                type: 'Nachthemd',
+                price: '69.90'
               }
             ]
           }
@@ -77,6 +81,7 @@ export default {
     },
 
     {
+      collapse: false,
       blocks: [
         {
           image: {
@@ -89,6 +94,40 @@ export default {
             url: '#'
           },
 
+          shoppingLayer: {
+            title: '',
+            inStock: true,
+            products: [
+              {
+                image: {
+                  url: '/static/images/desert-look_1.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'POM',
+                type: 'Dreiecktuch',
+                price: '89.90'
+              },
+              {
+                image: {
+                  url: '/static/images/desert-look_2.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'PRADA',
+                type: 'Sonnenbrille "0PR 68TS"',
+                price: '399.00'
+              },
+              {
+                image: {
+                  url: '/static/images/desert-look_3.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'GLOBUS ESSENTIALS',
+                type: 'Rock',
+                price: '179.90'
+              }
+            ]
+          },
+
           style: {
             padded: true
           }
@@ -97,6 +136,7 @@ export default {
     },
 
     {
+      collapse: true,
       blocks: [
         {
           title: {
@@ -110,6 +150,7 @@ export default {
     },
 
     {
+      collapse: false,
       blocks: [
         {
           content: {
@@ -122,37 +163,119 @@ export default {
           image: {
             url: '/static/images/california_make-up.png',
             alt: 'California Make-Up'
+          },
+
+          button: {
+            label: 'Make-Up ansehen',
+            url: '#'
+          },
+
+          shoppingLayer: {
+            title: '',
+            inStock: true,
+            products: [
+              {
+                image: {
+                  url: '/static/images/make-up_1.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'YSL',
+                type: 'Mascara Vinyl Couture',
+                price: '49.90'
+              },
+              {
+                image: {
+                  url: '/static/images/make-up_2.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'Clarins',
+                type: 'Eclat Minute Huile…',
+                price: '29.90'
+              },
+              {
+                image: {
+                  url: '/static/images/make-up_3.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'Shiseido',
+                type: 'Benefiance WrinkleResist24…',
+                price: '89.90'
+              }
+            ]
           }
         }
       ]
     },
 
     {
+      collapse: false,
       blocks: [
         {
-          image: {
-            url: '/static/images/california_sandals.png',
-            alt: ''
-          },
-
-          style: {
-            padded: true
-          }
-        }, {
           title: {
             label:
               'Ziersteine, Strass und Glitzer rücken den Fuss in den Fokus.',
-            align: 'left',
+            align: 'right',
             size: 'small',
             style: {
               backgroundColor: '#F2B661'
             }
           }
+        },
+        
+        {
+          image: {
+            url: '/static/images/california_sandals.png',
+            alt: ''
+          },
+          
+          button: {
+            label: 'Sandalen shoppen',
+            url: '#'
+          },
+
+          shoppingLayer: {
+            title: '',
+            inStock: true,
+            products: [
+              {
+                image: {
+                  url: '/static/images/sandalen_1.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'ALMA EN PENA',
+                type: 'Sandale',
+                price: '129.90'
+              },
+              {
+                image: {
+                  url: '/static/images/sandalen_2.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'MICHAEL KORS',
+                type: 'Sandale',
+                price: '169.00'
+              },
+              {
+                image: {
+                  url: '/static/images/sandalen_3.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'ALMA EN PENA',
+                type: 'Sandale',
+                price: '139.00'
+              }
+            ]
+          },
+
+          style: {
+            padded: true
+          }
         }
       ]
     },
 
     {
+      collapse: false,
       blocks: [
         {
           video: {
@@ -167,10 +290,12 @@ export default {
     },
 
     {
+      collapse: false,
       blocks: [
         {
           title: {
-            label: 'Valley Life'
+            label: 'Valley Life',
+            align: 'center'
           },
           
           image: {
@@ -187,7 +312,6 @@ export default {
 
     {
       collapse: true,
-
       blocks: [
         {
           image: {
@@ -196,15 +320,49 @@ export default {
           },
 
           button: {
-            label: 'Produkte anzeigen',
+            label: 'Skylights shoppen',
             url: '#'
+          },
+
+          shoppingLayer: {
+            title: '',
+            inStock: true,
+            products: [
+              {
+                image: {
+                  url: '/static/images/lampen_1.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'Calvin Klein',
+                type: 'Bikini Oberteil',
+                price: '79.90'
+              },
+              {
+                image: {
+                  url: '/static/images/lampen_2.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'FILA',
+                type: 'Athleisure-langarm Shirt',
+                price: '89.90'
+              },
+              {
+                image: {
+                  url: '/static/images/lampen_3.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'FILA',
+                type: 'Gürteltasche',
+                price: '19.90'
+              }
+            ]
           }
         },
 
         {
           content: {
             content: [
-              'Der Stoff, aus dem Wüstenträume sind — auch in den eigenen vier Wänden. Rattan-Lampen sind der Interior-Trend der Saison. Extraweiches Licht für milde Sommer-abende, in Größen bis zum 50 cm Umfang. Texmex-Style, grenzenlos.'
+              'Der Stoff, aus dem Wüstenträume sind — auch in den eigenen vier Wänden. Rattan-Lampen sind der Interior-Trend der Saison. Extraweiches Licht für milde Sommerabende, in Größen bis zum 50 cm Umfang. Texmex-Style, grenzenlos.'
             ]
           },
 
@@ -223,7 +381,6 @@ export default {
 
     {
       collapse: true,
-      
       blocks: [
         {
           image: {
@@ -234,6 +391,40 @@ export default {
           button: {
             label: 'Shop the look',
             url: '#'
+          },
+
+          shoppingLayer: {
+            title: '',
+            inStock: true,
+            products: [
+              {
+                image: {
+                  url: '/static/images/anzug_1.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'FIL NOIR',
+                type: 'Hemd tailliert',
+                price: '119.00'
+              },
+              {
+                image: {
+                  url: '/static/images/anzug_2.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'Michael Kors',
+                type: 'Strickpullover',
+                price: '159.00'
+              },
+              {
+                image: {
+                  url: '/static/images/anzug_3.jpg',
+                  alt: 'Dummy image'
+                },
+                title: 'L.B.M. 1911',
+                type: 'Anzug 2-teilig',
+                price: '699.00'
+              }
+            ]
           },
 
           style: {
