2497326362804cabdc65ed2a1d72a3168207b97f
Gustav Pursche <g.pursche@de.edenspiekermann.com>
2018-06-14 14:22:46 +0200

    Add share component

--- components/story/index.jsx
+++ components/story/index.jsx
@@ -1,12 +1,23 @@
-import { Fragment } from 'react';
-
 import Block from './block';
 import Intro from './intro';
+import Share from '../share';
 
 export default ({ intro, blocks }) => (
-  <Fragment>
+  <>
+    <style jsx>{`
+      .share {
+        margin-bottom: 60px;
+        margin-top: 60px;
+        text-align: center;
+      }
+    `}</style>
+
     <Intro {...intro} />
 
     {blocks.map(_ => <Block {..._} />)}
-  </Fragment>
+
+    <div className="share">
+      <Share />
+    </div>
+  </>
 );
